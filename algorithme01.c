f89a8dc3a4cc72a181c12a548fba5fad5c38c1f3
yuzukatane <yuzu.merci@gmail.com>
2022-09-20 17:11:20 +0900

    final ver.

--- algorithme01.c
+++ algorithme01.c
@@ -6,7 +6,7 @@
 /*   By: kyuzu <kyuzu@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/18 15:26:21 by kyuzu             #+#    #+#             */
-/*   Updated: 2022/09/18 15:38:24 by kyuzu            ###   ########.fr       */
+/*   Updated: 2022/09/20 17:10:43 by kyuzu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -130,72 +130,3 @@ void	is_larger(int len, int median, t_dlist **dst, t_dlist**src)
 			ra_rb(src, 'b');
 	}
 }
-
-/*
-int	send_half_small(int len, t_dlist **dst, t_dlist **src)
-{
-	int	median;
-	int	i;
-	int	flag;
-
-	median = (len % 2) + (len / 2);
-	i = 0;
-	while (i < median)
-	{
-		if ((*src)->index < median)
-		{
-			pa_pb(dst, src);
-			ft_printf("pb\n");
-			i++;
-		}
-		else
-		{
-			flag = top_or_bottom(src, median);
-			//無限ループになってる！下の行、関数書くまえはifではなくelse で書いてた
-			if (flag == TOP)
-			{
-				while ((*src)->index >= median)
-				{
-					ra_rb(src);
-					ft_printf("ra\n");
-				}
-			}
-			else if (flag == BOTTOM)
-			{
-				while ((*src)->index >= median)
-				{
-					rra_rrb(src);
-					ft_printf("rra\n");
-				}
-			}
-		}
-	}
-	return (median);
-}
-
-int	top_or_bottom(t_dlist **lst, int median)
-{
-	t_dlist *top;
-	t_dlist *bottom;
-	int		index_top;
-	int		index_bottom;
-
-	top = *lst;
-	bottom = (*lst)->prev->prev;
-	index_top = 0;
-	index_bottom = 0;
-	while (top->index >= median)
-	{
-		top = top->next;
-		index_top++;
-	}
-	while (bottom->index >= median)
-	{
-		bottom = bottom->next;
-		index_bottom++;
-	}
-	if (top->index < bottom->index)
-		return (TOP);
-	return (BOTTOM);
-}
-*/
